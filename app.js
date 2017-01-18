2a211e30a57a3449798bfa0651bf9b03bf45b89e
Chris Auret <chris.auret@yahoo.co.uk>
2017-01-18 14:35:54 +1100

    move ship

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ function setup() {
 
 function draw() {
   background(0);
-
-  ship.render();
+  ship.turn();
+    ship.render();
 }
+
+function keyReleased(){
+  ship.setRotation(0);
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW){
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW){
+    ship.setRotation(-0.1);
+  }
+}
+
--- ship.js
+++ ship.js
@@ -2,15 +2,21 @@ function Ship() {
 
   this.pos = createVector(width/2, height/2);
   this.r = 10;
+  this.heading = 0;
+  this.rotation = 0;
 
   this.render  = function() {
-
     translate(this.pos.x, this.pos.y);
-
-    var c = color(255, 204, 0); 
-    fill(c);
-    noStroke();
-
+    var c = color(255, 204, 0);  fill(c); noStroke();
+    rotate(this.heading);    
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+  }
+
+  this.turn = function() {
+    this.heading += this.rotation;
+  }
+
+  this.setRotation = function(a){
+    this.rotation = a;
   }
 }
